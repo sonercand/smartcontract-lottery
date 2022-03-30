3a2e380ab3706ffca2c259ef662fec356e76cd55
sonercand <s.ism.candan@gmail.com>
2022-03-30 11:43:11 +0000

    add OpenZeppelin ownable

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,20 +1,29 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lottery {
+contract Lottery is Ownable {
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
 
     constructor(address _pricefeedAddress) public {
         // 50 dollars min
         usdEntryFee = 50 * (10**18);
         ethUsdPriceFeed = AggregatorV3Interface(_pricefeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
     }
 
     function enter() public payable {
-        //require();
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value >= getEnteranceFee(), "Not enough to participate");
         players.push(payable(msg.sender));
     }
 
@@ -26,7 +35,10 @@ contract Lottery {
         return costToEnter;
     }
 
-    function startLottery() public {}
+    function startLottery() public onlyOwner {
+        require(lottery_state == LOTTERY_STATE.CLOSED);
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
 
     function endLotter() public {}
 }
